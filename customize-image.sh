8dd46567169ca635282efea320fc1b493f4313d4
vagrant <vagrant@vagrant.vm>
2019-03-23 11:03:44 +0000

    minor customize update

--- customize-image.sh
+++ customize-image.sh
@@ -36,7 +36,7 @@ case $RELEASE in
 	# Install Ethereum client packages
 	echo "Installing Parity and Geth Debian packages"	
 	dpkg -i /tmp/overlay/geth_1.8.23-0_arm64.deb
-        dpkg -i /tmp/overlay/parity_2.3.5-0_arm64.deb
+        dpkg -i /tmp/overlay/parity_2.3.8-0_arm64.deb
 	dpkg -i /tmp/overlay/ipfs_0.4.18-0_arm64.deb
         dpkg -i /tmp/overlay/status.im-node-0.19.0-beta3_arm64.deb
 	dpkg -i /tmp/overlay/raiden_0.100.2-0_arm64.deb
@@ -58,7 +58,7 @@ case $RELEASE in
 	cp -f /tmp/overlay/cpufrequtils /etc/default
         # Install Ethereum client packages
         echo "Installing Parity and Geth Debian packages"       
-        dpkg -i /tmp/overlay/parity_2.3.5-0_arm64.deb
+        dpkg -i /tmp/overlay/parity_2.3.8-0_arm64.deb
 	dpkg -i /tmp/overlay/geth_1.8.23-0_arm64.deb
 	dpkg -i /tmp/overlay/ipfs_0.4.18-0_arm64.deb
 	dpkg -i /tmp/overlay/status.im-node-0.19.0-beta3_arm64.deb
