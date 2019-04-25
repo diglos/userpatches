bc49744d3b1f6255d5e4b5aff8b36f4297ab112a
diglos <diego.losadaf@gmail.com>
2019-04-25 10:38:29 +0000

    added unattended upgrades

--- customize-image.sh
+++ customize-image.sh
@@ -49,14 +49,20 @@ case $RELEASE in
 	# you can copy the above code here if you prefer Ubuntu instead of Debian
 	;;
 	bionic)
-	# Manage hostname, ethereum account and SSD device on first boot
+	# Manage NVMe drive, create ethereum account and change hostname on first boot
         cp /tmp/overlay/armbian_first_run.txt /boot
         cp -f /tmp/overlay/rc.local /etc
         cp -f /tmp/overlay/first_reboot.sh /usr/local/bin
+	# Install Ethereum update script
 	cp -f /tmp/overlay/update-ethereum /usr/local/bin
+	# Install Trinity client script
 	cp -f /tmp/overlay/install-trinity /usr/local/bin
+	# Limit cpu frequency to prevent CPU throttling
 	cp -f /tmp/overlay/cpufrequtils /etc/default
-        # Install Ethereum client packages
+	# Configure unattended upgrades
+	cp -f /tmp/overlay/02-armbian-periodic /etc/apt/apt.conf.d/
+	cp -f /tmp/overlay/50unattended-upgrades /etc/apt/apt.conf.d/
+        # Install Ethereum packages
         echo "Installing Parity and Geth Debian packages"       
 	dpkg -i /tmp/overlay/geth_1.8.27-0_arm64.deb
 	dpkg -i /tmp/overlay/parity_2.4.5-0_arm64.deb
