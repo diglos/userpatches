10522963aa8f0eeacae061cf573e9c9f5b0ced1c
vagrant <vagrant@vagrant.vm>
2019-09-07 15:06:09 +0000

    restored geth as default client

--- customize-image.sh
+++ customize-image.sh
@@ -61,7 +61,7 @@ EOF
 	add-apt-repository "deb http://apt.ethraspbian.com bionic-upgrades main"
 	# Install Ethereum packages
 	apt-get install geth
-	apt-get install swarm parity ipfs raiden status.im-node vipnode prysm-beacon prysm-validator
+	apt-get install parity swarm parity ipfs raiden status.im-node vipnode prysm-beacon prysm-validator
 	# Install ATS script for handling fan activiy and disable ZRAM on rockpro64 to avoid RAM issues
 	if [ "$BOARD" == "rockpro64" ];
 	then
